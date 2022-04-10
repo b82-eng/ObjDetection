2bdc49dd3c63c2fe6c44e705e4eca3b041ca10bf
brownt01 <103010015+agent2x0r@users.noreply.github.com>
2022-04-10 17:30:07 -0400

    Create StereoCameras.py
    Access two USB cameras with OpenCV, read the frames in while(1) loop. Display individual frames, and single overlapping frame to assist with course alignment of cameras.
    
    TODO: Grayscale conversion, Image Rectification, Disparity Map

---
+++ Source/StereoCameras.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy
+print(cv2.__version__)
+
+
+## Camera Object
+# webcam = cv2.VideoCapture(0)
+camLeft = cv2.VideoCapture(1)
+camRight = cv2.VideoCapture(2)
+
+# Ensure at least one camera can be opened 
+if not (camRight.isOpened() | camLeft.isOpened()):
+    print("Could not open a video device")
+
+## Infinite Loop
+while(True):
+        
+    # Capture frame-by-frame    
+    ret, frame = camRight.read() 
+    ret2, frame2 = camLeft.read()    
+
+    # Dispaly the individual camera frames
+    cv2.imshow('frame', frame)
+    cv2.imshow('frame2', frame2)
+
+
+    # Combine the two frames, 50% weight to each to visualize the disparity and perform manual coarse alignment 
+    dst = cv2.addWeighted(frame, 0.5, frame2, 0.5,0)
+    cv2.imshow('dst',dst)
+
+    ## -- BELOW IS Work In Progress -- ##
+
+    # Convert frames to Grayscale 
+    grayLeft = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    grayRight = cv2.cvtColor(frame2, cv2.COLOR_RGB2GRAY)
+
+    cv2.imshow('left', grayLeft)
+
+    ## 
+    ## TODO: Perform Rectification so copmarable pixels are on the same horizonal / epipolar line
+    ##
+
+
+    ##
+    ## CV Disparity Map
+    ##
+
+    #stereo = cv2.StereoBM_create(numDisparities=15, blockSize=15)
+    #disparity = stereo.compute(grayLeft, grayRight)
+    #cv2.imshow('disparity', disparity)
+
+    ## -- ABOVE IS Work In Progress -- ##
+
+    # Waits for a user input (the letter 'q') to quit the application    
+    if cv2.waitKey(1) & 0xFF == ord('q'):    
+        break
+
+
+# Release the cameras and close windows
+camLeft.release() 
+camRight.release()
+cv2.destroyAllWindows()

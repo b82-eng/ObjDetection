f05cbccc22a8630dad06cdfe4911b905807643a1
Benjamin Edwards <edwaba02@pfw.edu>
2022-04-10 16:07:30 -0400

    Rendering two video streams
    Modified the code to process two streams instead of one

--- Source/Test.py
+++ Source/Test.py
@@ -5,16 +5,16 @@ from windowcapture import list_window_names
 
 list_window_names()
 
-cap = cv.VideoCapture(1) # Camera 1 (Integrated webcam)
-cap2 = cv.VideoCapture(0) # Camera 2 (Logitech webcam)
+cap = cv.VideoCapture(0) # Camera 1 (Integrated webcam)
+#cap2 = cv.VideoCapture(0) # Camera 2 (Logitech webcam)
 
 while(1):
     grabbed, frame = cap.read() # Read frame from webcam
-    grabbed2, frame2 = cap2.read() # Read frame from Logitech
+    #grabbed2, frame2 = cap2.read() # Read frame from Logitech
     #print(grabbed)
     #print(frame)
     cv.imshow('frame',frame) # Display frame from webcam
-    cv.imshow('frame2', frame2) # Display frame from Logitech
+    #cv.imshow('frame2', frame2) # Display frame from Logitech
 
     # press 'q' with the output window focused to exit
     # waits 1 ms every loop to process key presses
@@ -25,4 +25,4 @@ while(1):
     #time.sleep(5)
 
 cap.release()
-cap2.release()
+#cap2.release()
--- cone-detector-tf-master/cone-detector-tf-master/cone_detector.py
+++ cone-detector-tf-master/cone-detector-tf-master/cone_detector.py
@@ -17,11 +17,13 @@ logging.config.fileConfig('logging.ini')
 
 #VIDEO_PATH = 'testdata/sample_video.mp4'
 VIDEO_PATH = 'testdata/autocross_test.mp4'
+#VIDEO_PATH2 = 'testdata/autocross_test.mp4'
 FROZEN_GRAPH_PATH = 'models/ssd_mobilenet_v1/frozen_inference_graph.pb'
 
 OUTPUT_WINDOW_WIDTH = 640  # Use None to use the original size of the image
 #DETECT_EVERY_N_SECONDS = None  # Use None to perform detection for each frame
-DETECT_EVERY_N_SECONDS = 0.1  # Use None to perform detection for each frame
+DETECT_EVERY_N_SECONDS = 0.2  # Use None to perform detection for each frame
+#DETECT_EVERY_N_SECONDS = 1  # Use None to perform detection for each frame
 
 # TUNE ME
 CROP_WIDTH = CROP_HEIGHT = 600
@@ -38,23 +40,28 @@ def main():
 
     # Read video from disk and count frames
     cap = cv2.VideoCapture(VIDEO_PATH)
+    cap2 = cv2.VideoCapture(VIDEO_PATH)
 
     fps = cap.get(cv2.CAP_PROP_FPS)
+    #fps2 = cap2.get(cv2.CAP_PROP_FPS)
 
-    # CROP_WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # CROP_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    #CROP_WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #CROP_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     with tf.Session(graph=detection_graph) as sess:
 
         processed_images = 0
-        while cap.isOpened():
+        while (cap.isOpened() and cap2.isOpened()):
 
             if DETECT_EVERY_N_SECONDS:
                 cap.set(cv2.CAP_PROP_POS_FRAMES,
                         processed_images * fps * DETECT_EVERY_N_SECONDS)
+                cap2.set(cv2.CAP_PROP_POS_FRAMES,
+                        processed_images * fps * DETECT_EVERY_N_SECONDS)
 
             ret, frame = cap.read()
-            if ret:
+            ret2, frame2 = cap2.read()
+            if (ret and ret2):
                 tic = time.time()
 
                 # crops are images as ndarrays of shape
@@ -65,12 +72,17 @@ def main():
                     frame, CROP_HEIGHT, CROP_WIDTH,
                     CROP_STEP_VERTICAL, CROP_STEP_VERTICAL)
 
+                crops2, crops_coordinates2 = ops.extract_crops(
+                    frame2, CROP_HEIGHT, CROP_WIDTH,
+                    CROP_STEP_VERTICAL, CROP_STEP_VERTICAL)
+
                 # Uncomment this if you also uncommented the two lines before
                 #  creating the TF session.
-                # crops = np.array([crops[0]])
-                # crops_coordinates = [crops_coordinates[0]]
+                #crops = np.array([crops[0]])
+                #crops_coordinates = [crops_coordinates[0]]
 
                 detection_dict = tf_utils.run_inference_for_batch(crops, sess)
+                detection_dict2 = tf_utils.run_inference_for_batch(crops2, sess)
 
                 # The detection boxes obtained are relative to each crop. Get
                 # boxes relative to the original image
@@ -85,13 +97,29 @@ def main():
                 if boxes:
                     boxes = np.vstack(boxes)
 
+                boxes2 = []
+                for box_absolute, boxes_relative in zip(
+                        crops_coordinates2, detection_dict2['detection_boxes']):
+                    boxes2.extend(ops.get_absolute_boxes(
+                        box_absolute,
+                        boxes_relative[np.any(boxes_relative, axis=1)]))
+                if boxes2:
+                    boxes2 = np.vstack(boxes2)
+
                 # Remove overlapping boxes
                 boxes = ops.non_max_suppression_fast(
                     boxes, NON_MAX_SUPPRESSION_THRESHOLD)
 
+                boxes2 = ops.non_max_suppression_fast(
+                    boxes2, NON_MAX_SUPPRESSION_THRESHOLD)
+
                 # Get scores to display them on top of each detection
                 boxes_scores = detection_dict['detection_scores']
                 boxes_scores = boxes_scores[np.nonzero(boxes_scores)]
+
+                # Get scores to display them on top of each detection
+                boxes_scores2 = detection_dict2['detection_scores']
+                boxes_scores2 = boxes_scores2[np.nonzero(boxes_scores2)]
 
                 for box, score in zip(boxes, boxes_scores):
                     if score > SCORE_THRESHOLD:
@@ -103,11 +131,26 @@ def main():
                             frame, ymin, xmin, ymax, xmax,
                             color_detected_rgb, text)
 
+                for box, score in zip(boxes2, boxes_scores2):
+                    if score > SCORE_THRESHOLD:
+                        ymin, xmin, ymax, xmax = box
+                        color_detected_rgb = cv_utils.predominant_rgb_color(
+                            frame2, ymin, xmin, ymax, xmax)
+                        text = '{:.2f}'.format(score)
+                        cv_utils.add_rectangle_with_text(
+                            frame2, ymin, xmin, ymax, xmax,
+                            color_detected_rgb, text)
+
                 if OUTPUT_WINDOW_WIDTH:
                     frame = cv_utils.resize_width_keeping_aspect_ratio(
                         frame, OUTPUT_WINDOW_WIDTH)
 
+                if OUTPUT_WINDOW_WIDTH:
+                    frame2 = cv_utils.resize_width_keeping_aspect_ratio(
+                        frame2, OUTPUT_WINDOW_WIDTH)
+
                 cv2.imshow('Detection result', frame)
+                cv2.imshow('Detection result 2', frame2)
                 cv2.waitKey(1)
                 processed_images += 1
 
@@ -122,6 +165,7 @@ def main():
                 break
 
     cap.release()
+    cap2.release()
     cv2.destroyAllWindows()
 
 
